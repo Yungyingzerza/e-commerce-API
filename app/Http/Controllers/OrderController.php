5a82c7b5552aee9f9d3366627832a9980ee9990e
TheUnknown550 <mattcosh06@gmail.com>
2025-03-03 14:11:24 +0700

    Fix User Auth

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function order(Request $request)
+    {
+        // Order a product
+    }
+
+    public function store(Request $request)
+    {
+        // Create a new order
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Update an existing order
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        // Delete an order
+    }
+}
